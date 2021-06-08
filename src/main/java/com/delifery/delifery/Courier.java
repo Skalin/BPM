a3775d4ce24687e4fb40ec084cf9f9bf3b31b833
wbadmin <>
2021-06-08 19:41:01 +0200

    {/src/main/java/com/delifery/delifery/Courier.java}

--- src/main/java/com/delifery/delifery/Courier.java
+++ src/main/java/com/delifery/delifery/Courier.java
@@ -6,12 +6,44 @@ package com.delifery.delifery;
 
 public class Courier implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Courier() {
-    }
+	private java.lang.Integer id;
+	private java.lang.String name;
+	private java.lang.String car;
 
+	public Courier() {
+	}
 
+	public java.lang.Integer getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getCar() {
+		return this.car;
+	}
+
+	public void setCar(java.lang.String car) {
+		this.car = car;
+	}
+
+	public Courier(java.lang.Integer id, java.lang.String name,
+			java.lang.String car) {
+		this.id = id;
+		this.name = name;
+		this.car = car;
+	}
 
 }
